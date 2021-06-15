3308614e9f7e81fbca5face840e3d43bde272879
hoangthetai1612 <hoangthetai16121998@gmail.com>
2021-06-15 11:59:27 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
-    path: 'home',
+    path: 'home/:id',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 ];
