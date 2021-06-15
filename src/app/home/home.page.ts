70f347b50ced562c17abd23d4c590ff738915a57
hoangthetai1612 <hoangthetai16121998@gmail.com>
2021-06-15 11:06:12 +0700

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  model = {
+    status: false,
+    fullName: 'Hoa Van Nguyen',
+    dob: '06/20/1961',
+    firstDose: '12/17/2020',
+    secondDose: '01/07/2021',
+    manufacture: 'Pfizer-BioNTech'
 
+  }
   constructor() { }
 
   ngOnInit() {
